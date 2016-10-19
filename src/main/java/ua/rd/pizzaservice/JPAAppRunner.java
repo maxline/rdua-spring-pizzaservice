296d9395358869db97f8271d561747734bc26f38
maxline <sergml@mail.ru>
2016-10-19 17:40:51 +0300

    v12 @GeneratedValue(strategy = GenerationType) example

--- src/main/java/ua/rd/pizzaservice/JPAAppRunner.java
+++ src/main/java/ua/rd/pizzaservice/JPAAppRunner.java
@@ -21,7 +21,8 @@ public class JPAAppRunner {
         EntityManagerFactory emf = Persistence.createEntityManagerFactory("jpa");  //персистенс юнит который мы прописали в xml
         EntityManager em = emf.createEntityManager();
 
-        Pizza pizza = new Pizza(3, "Bavarian", Pizza.PizzaType.MEAT, new BigDecimal("0.00"));
+        Pizza pizza = new Pizza(null, "Bavarian", Pizza.PizzaType.MEAT, new BigDecimal("0.00"));
+        Pizza pizza2 = new Pizza(null, "Bavarian2", Pizza.PizzaType.MEAT, new BigDecimal("0.00"));
         //надо получить ентити менеджер, это как процедурное программирование, передаем параметр объект, и он с ним что-то делает
         //также можно передавать запросы, как нативные так и gpql
         //управляет жизненным циклом объекта
@@ -33,9 +34,20 @@ public class JPAAppRunner {
 
         EntityTransaction transaction = em.getTransaction();
         transaction.begin();
+        System.out.println("Before persist " + pizza.getId());
+
         em.persist(pizza);  //сохраняет объект, кладет в Entity context, не в базу, по этому нужно создать транзакцию
-        transaction.commit();
-        em.clear(); //чтобы подтягивал из базы а не из менеджера
+        em.persist(pizza2); //два селекта будет, не оптимально (если знает что будет много сохраняться, делать диапазон   @SequenceGenerator сколько за раз брать
+        em.flush(); // надо сохранить на сейчас
+        pizza.setName("Hawai");  //сохраняет состояние на момент коммита! а не персист
+        System.out.println(pizza.getId());
+
+        //transaction.rollback();
+        transaction.commit(); // update будет делаться на коммите
+
+
+
+        //em.clear(); //чтобы подтягивал из базы а не из менеджера
         Pizza p = em.find(Pizza.class, 3);
         System.out.println(p == pizza);
 
--- src/main/java/ua/rd/pizzaservice/domain/Pizza.java
+++ src/main/java/ua/rd/pizzaservice/domain/Pizza.java
@@ -1,6 +1,8 @@
 package ua.rd.pizzaservice.domain;
 
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import java.io.Serializable;
 import java.math.BigDecimal;
@@ -9,13 +11,16 @@ import java.math.BigDecimal;
  * @author andrii
  */
 @Entity
-public class Pizza implements Serializable{
+public class Pizza implements Serializable {
 
     public enum PizzaType {
         VEGETARIAN, SEA, MEAT;
     }
 
-    @Id
+    @Id @GeneratedValue(strategy = GenerationType.TABLE)   //создается отельная табличка hybernate_sequences, можно делать несколько сиквенсов, в табличке разными строчками
+    //@Id @GeneratedValue(strategy = GenerationType.SEQUENCE)
+//    @SequenceGenerator(name = "PizzaSEQ", allocationSize = 10)
+//    @Id @GeneratedValue(strategy = GenerationType.AUTO, generator = "PizzaSEQ")
     private Integer id;
     private String name;
     private PizzaType pizzaType;
@@ -23,6 +28,10 @@ public class Pizza implements Serializable{
 
 
     public Pizza() {
+    }
+
+    public Integer getId() {
+        return id;
     }
 
     public Pizza(Integer id, String name, PizzaType pizzaType, BigDecimal price) {
@@ -34,6 +43,10 @@ public class Pizza implements Serializable{
 
     public BigDecimal getPrice() {
         return price;
+    }
+
+    public void setName(String name) {
+        this.name = name;
     }
 
     @Override
