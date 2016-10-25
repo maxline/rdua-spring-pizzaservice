89a7a5156cf6ef4370045edca4d84f6389790225
maxline <sergml@mail.ru>
2016-10-25 14:39:07 +0300

    v15_4 - Rodionov   spring JPA hibernate юнит тест вынесли аннотации в другой класс и заэкстендились от него

--- src/test/java/ua/rd/pizzaservice/repository/JPAPizzaRepositoryTest.java
+++ src/test/java/ua/rd/pizzaservice/repository/JPAPizzaRepositoryTest.java
@@ -1,38 +1,27 @@
 package ua.rd.pizzaservice.repository;
 
 import org.junit.Test;
-import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.annotation.Rollback;
-import org.springframework.test.context.ContextConfiguration;
-import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.transaction.annotation.Transactional;
 import ua.rd.pizzaservice.domain.Pizza;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertNotNull;
 
-//будет использовать не свой раннер а чужой, для того чтобы поднять спринговый контейнер
-@RunWith(SpringJUnit4ClassRunner.class)
-@ContextConfiguration(locations = {"classpath:/repoContext.xml"})
-public class JPAPizzaRepositoryTest {
+public class JPAPizzaRepositoryTest extends RepositoryTestConfig {
 
     @Autowired
     private PizzaRepository pizzaRepository;
 
-    //нужно чтобы во время этого теста поднялся реальный спринговый контейнер
     @Test
     @Transactional
     @Rollback(true)
     public void save() throws Exception {
-
         Pizza pizza = new Pizza();
-
         pizza.setName("Sea");
         pizza.setPizzaType(Pizza.PizzaType.SEA);
-
         pizza = pizzaRepository.save(pizza);
 
         assertNotNull(pizza.getId());
     }
-
 }
