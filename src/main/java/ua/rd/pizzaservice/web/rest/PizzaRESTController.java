c26e0c40ac8542a77829208c7785f334b9df0ff4
maxline <sergml@mail.ru>
2016-11-02 16:14:38 +0200

    v20_7.  rodionov. @RestController // чтобы не писать над каждым методом ResponseBody RESTContext.xml деприкатед аннотацию убрали DefaultAnnotationHandlerMapping <mvc:annotation-driven/>

--- src/main/java/ua/rd/pizzaservice/web/rest/PizzaRESTController.java
+++ src/main/java/ua/rd/pizzaservice/web/rest/PizzaRESTController.java
@@ -1,25 +1,24 @@
 package ua.rd.pizzaservice.web.rest;
 
 
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
 
 /**
  * @author Serhii_Mykhliuk
  */
 
-@Controller
+@RestController // чтобы не писать над каждым методом ResponseBody
 public class PizzaRESTController {
     //можно специфицировать много параметров, например только гет запрос
-    @RequestMapping(value = "/hello", method = {RequestMethod.GET, RequestMethod.POST})
-    @ResponseBody
-    public String hello(){
+    @RequestMapping(value = "/hello", method = RequestMethod.GET)
+    public String hello() {
         // http://localhost:8081/s-pizza-service/rest/hello
         //текст это не вью, а просто хотим вернуть в респонс, будет как output writer
         return "<b>Hello from REST Controller!</b>";
     }
+
 
 }
